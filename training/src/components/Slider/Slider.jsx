0d0856f9699f3a062719b2012a8800556aef6a27
bhavya-goel <bhavya.goel@successive.tech>
2019-09-20 13:03:39 +0530

    feat: states

---
+++ training/src/components/Slider/Slider.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { getNextRoundRobin, getRandomNumber } from '../../lib/utils/math'
+import { PUBLIC_IMAGE_FOLDER, DEFAULT_BANNER_IMAGE } from '../../configs/constants'
+class Slider extends Component {
+    constructor(props) {
+        super(props)
+        this.state={
+            bannerNumber: 0,
+            source: props.banners[0] || props.defaultBanner,
+            altText: props.altText || 'Default Banner',
+            duration: props.duration || 2000,
+            height: props.height || 200
+        }
+
+        this.changeBanner = this.changeBanner.bind(this)
+    }
+
+    componentDidMount() {
+        let time = this.state.duration
+        setInterval(this.changeBanner, time)
+    }
+
+    changeBanner() {
+        const current = this.state.bannerNumber
+        const index = this.props.random ? 
+        getRandomNumber(5) :
+        getNextRoundRobin(6, current)
+        this.setState({
+            bannerNumber: index,
+            source: this.props.banners[index]
+        })
+    }
+
+    render() {
+        return (
+            <img src={PUBLIC_IMAGE_FOLDER + this.state.source}
+            alt={this.state.altText}
+            style={{
+                margin: 'auto',
+                display: 'block',
+                height: `${this.state.height}px`
+                }}
+            />
+        )
+    }
+}
+
+export default Slider
--- training/src/components/pages/TextFieldDemo/TextFieldDemo.jsx
+++ training/src/components/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import { TextField } from '../../TextField'
+import { Slider } from '../../Slider'
 
 function TextFieldDemo(props) {
     return(
         <div>
+            <div>
+                <Slider
+                    altText='Default Banner'
+                    banners={
+                        [
+                            'default.png',
+                            'load-balancer.png',
+                            'full-stack-web-development.jpg',
+                            'js.jpg',
+                            'dns-server.png',
+                            'cloud.jpg'
+                        ]
+                    }
+                    defaultBanner='default.png'
+                    duration={1000}
+                    height={150}
+                    random={false}
+                />
+            </div>
+            <br/>
             <TextField value='text field'/>
             <TextField error='error field'/>
             <TextField disabled={true} />
