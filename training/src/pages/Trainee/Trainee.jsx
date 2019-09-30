fa54b3aee17ced96fff53cca1b3750772d90c105
bhavya-goel <bhavya.goel@successive.tech>
2019-09-30 12:42:58 +0530

    feat: add new components

--- training/src/pages/Trainee/Trainee.jsx
+++ training/src/pages/Trainee/Trainee.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import * as yup from 'yup'
-import { Navbar } from '../components'
+import { Navbar } from '../../layouts'
 import { AddDialog } from './components'
 import { Button } from '@material-ui/core'
 
