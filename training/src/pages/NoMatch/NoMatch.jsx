41e5160779b951dec9ea9bc50254aa32f6d4c761
bhavya-goel <bhavya.goel@successive.tech>
2019-09-30 13:06:11 +0530

    feat: add noMatch component

---
+++ training/src/pages/NoMatch/NoMatch.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+function NoMatch() {
+    return(
+        <div style={{textAlign: 'center'}}>
+            <h1>
+                Not Found
+            </h1>
+            <h3>
+                Seems like the page you are looking after does not exist
+            </h3>
+        </div>
+    )
+}
+export default NoMatch
